b85ba2006af009e91460d1966e6b850a913daa77
Aman Sarounia <amansarounia12@gmail.com>
2025-09-05 17:15:18 +0530

    Add refreshAccessToken endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+
 dotenv.config({ path: "./.env" });
 
 const app = express(); // <-- you forgot this line
@@ -17,9 +18,9 @@ app.get("/", (req, res) => {
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(` Server is running at port: ${process.env.PORT || 8000}`);
+      console.log(`✅ Server is running at port: ${process.env.PORT || 8000}`);
     });
   })
   .catch((error) => {
-    console.log(" MONGO db connection failed !!!", error);
+    console.log("❌ MONGO db connection failed !!!", error);
   });
